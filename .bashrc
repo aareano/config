a87ff0d8baf6828c81b5f5d04985b629627d92fe
Aaron Bowen <abowen@hubspot.com>
2021-01-10 15:15:53 -0800

    changes

--- .bashrc
+++ .bashrc
@@ -1,7 +1,18 @@
+export PATH=~/bin:$PATH
+
+# powerline-shell
 function _update_ps1() {
     PS1=$(powerline-shell $?)
 }
-
 if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
+
+# iterm2 python scripts
+function iterm2_print_user_vars() {
+  iterm2_set_user_var gitBranch $((git branch 2> /dev/null) | grep \* | cut -c3-)
+  iterm2_set_user_var home $(echo -n "$HOME")
+}
+
+# git auto-completion
+source ~/.git-completion
--- kickstarter.sh
+++ kickstarter.sh
@@ -3,7 +3,7 @@
 # Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
-    echo "Installing Homebrew."
+    echo "Installing Homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     echo "Homebrew is installed."
@@ -11,7 +11,7 @@ fi
 
 which -s pip3
 if [[ $? != 0 ]] ; then
-    echo "Installing pip3."
+    echo "Installing pip3..."
     brew install pip3
 else
     echo "pip3 is installed."
@@ -19,10 +19,17 @@ fi
 
 which -s powerline-shell
 if [[ $? != 0 ]] ; then
-    echo "Installing powerline-shell."
+    echo "Installing powerline-shell..."
     brew install powerline-shell
 else
     echo "powerline-shell is installed."
 fi
 
-echo "Now copy over the different configs you want."
+if [ -f ~/.git-completion ]; then
+    echo "~/.git-completion exists"
+else
+    echo "Setting up git auto-completion..."
+    curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ~/.git-completion
+fi
+
+echo "Now copy over the different configs you want from the folders in this repo."
